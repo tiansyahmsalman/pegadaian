6d595c82f0f9b0281eefd640e0ed47371fd445b2
dave <dave.prastya@gmail.com>
2021-09-28 13:13:35 +0700

    refactor(tab-detail): change into detail and historical

--- pages/view/account.js
+++ pages/view/account.js
@@ -12,6 +12,7 @@ export default function Account() {
   const user = navbarContext.user;
 
   function handleClick() {
+    navbarContext.updateActiveNavbar("Home");
     localStorage.removeItem("token");
     router.push("/view/login");
   }
