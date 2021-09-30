ca8406459bbea7e464d641f4d999556f2ac005be
dave <dave.prastya@gmail.com>
2021-09-30 22:04:31 +0700

    feature(decrypt): implement custom decrypt into context

--- helper/convertDateTime.js
+++ helper/convertDateTime.js
@@ -1,5 +1,10 @@
 export default function convertDateTime(payload) {
-  const date = new Date(payload).toISOString().split('T')
-  const time = date[1].split('.')[0]
-  return `${date[0]} ${time}`
+  if (!payload || payload.includes('not ') || payload.includes('Not ')) {
+    return payload;
+  } else {
+    const date = new Date(payload).toISOString().split('T')
+    const time = date[1].split('.')[0]
+    return `${date[0]} ${time}`
+  }
+  
 }
