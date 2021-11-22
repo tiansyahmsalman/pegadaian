e0cd1d67e61c025b020800dbbfeb90322ed14bc5
zaini01 <o.z41n1@gmail.com>
2021-11-22 13:13:28 +0700

    fix wording listProductRecommendation

--- components/list.js
+++ components/list.js
@@ -14,8 +14,8 @@ export default function List(props) {
           <strong>{data.encNama}</strong>
           <Text>{data.encNoHandphone}</Text>
           {
-            !data.listProductRecommendation || data.listProductRecommendation.includes('not ') || data.listProductRecommendation.includes('Not ') ? 
-            (<Text>{'Rekomendasi: Tidak ada'}</Text>) : (<Text>{`Rekomendasi: ${data.listProductRecommendation.toString().replaceAll(",", ", ")}`}</Text>)
+            !data.listProductRecommendation || data.listProductRecommendation.includes('not ') || data.listProductRecommendation.includes('Not ') || data.listProductRecommendation.includes('') ? 
+            (<Text>{'Rekomendasi: Saat ini belum ada data tersedia untuk cabang anda'}</Text>) : (<Text>{`Rekomendasi: ${data.listProductRecommendation.toString().replaceAll(",", ", ")}`}</Text>)
           }
         </Container>
       </Flex>
